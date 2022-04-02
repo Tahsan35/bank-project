89d95c5d5089467be21c9967e5083e95b3fa56df
Tahsan35 <tahsanknmas14@gmail.com>
2022-04-02 15:14:52 +0600

    finishe

--- Banking/banking.js
+++ Banking/banking.js
@@ -25,10 +25,33 @@ const previousBalanceTotal = parseFloat(balanceTotalText);
 const newBalanceTotal = previousBalanceTotal + newDepositAmount;
 balanceTotal.innerText = newBalanceTotal;
 
-
-
-
-
     // clear the deposit input field
     depositInput.value = " "; 
-}) 
+});
+
+// handle withdraw even handler
+document.getElementById("withdraw-button").addEventListener("click",function() {
+    const withdrawInput = document.getElementById("withdraw-input");
+    const withdrawAmountText = withdrawInput.value;
+    const newWithdrawAmount = parseFloat(withdrawAmountText);
+
+    // set withdraw total
+    const withdrawTotal = document.getElementById("withdraw-total");
+    const previousWithdrawText = withdrawTotal.innerText;
+    const previousWithdrawTotal = parseFloat (previousWithdrawText);
+
+    const newWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+    withdrawTotal.innerText = newWithdrawTotal;
+
+    // update balance
+    const balanceTotal = document.getElementById("balance-total");
+    const previousBalanceText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat (previousBalanceText);
+    const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+    balanceTotal.innerText = newBalanceTotal;
+
+    // clear the withdraw input field
+    withdrawInput.value = " "; 
+
+    // console.log(previousWithdrawTotal);
+})
